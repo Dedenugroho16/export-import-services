6d88037d7f00d00a7a1b6e32fff9d2b1df230903
Fazri Setia <fazrisetiaramadan@gmail.com>
2024-09-06 23:24:05 +0700

    Menambah route ke client

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -48,14 +48,19 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./form-elements.html" >
-                  <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/checkbox -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 11l3 3l8 -8" /><path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" /></svg>
-                  </span>
-                  <span class="nav-link-title">
-                    Forms
-                  </span>
-                </a>
+                  <a class="nav-link" href="{{ url('/clients') }}">
+                      <span class="nav-link-icon d-md-none d-lg-inline-block">
+                          <!-- Download SVG icon from http://tabler-icons.io/i/user -->
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                              <circle cx="12" cy="7" r="4" />
+                              <path d="M5 21v-4a7 7 0 0 1 14 0v4" />
+                          </svg>
+                      </span>
+                      <span class="nav-link-title">
+                          Clients
+                      </span>
+                  </a>
               </li>
             </ul>
           </div>
@@ -63,41 +68,6 @@
       </aside>
 
       <div class="page-wrapper">
-        <!-- Page header -->
-        <div class="page-header d-print-none">
-          <div class="container-xl">
-            <div class="row g-2 align-items-center">
-              <div class="col">
-                <!-- Page pre-title -->
-                <div class="page-pretitle">
-                  Overview
-                </div>
-                <h2 class="page-title">
-                  Fluid vertical layout
-                </h2>
-              </div>
-              <!-- Page title actions -->
-              <div class="col-auto ms-auto d-print-none">
-                <div class="btn-list">
-                  <span class="d-none d-sm-inline">
-                    <a href="#" class="btn">
-                      New view
-                    </a>
-                  </span>
-                  <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-                    Create new report
-                  </a>
-                  <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <!-- Page body -->
         <div class="content">
           @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Rute untuk halaman dashboard
 Route::get('/', function () {
     return view('dashboard');
+});
+
+// Rute untuk halaman clients
+Route::get('/clients', function () {
+    return view('clients');
 });
