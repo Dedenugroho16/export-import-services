8448297ef5409fc29b9d42326ef1e1a2a714506b
Dede Nugroho <dedenugrohomm2@gmail.com>
2024-09-11 08:28:05 +0700

    Formatting

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,93 +1,104 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Sign up</title>
     <!-- CSS files -->
-    <link href="./dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
-    <link href="./dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
-    <link href="./dist/css/tabler-payments.min.css?1692870487" rel="stylesheet"/>
-    <link href="./dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet"/>
-    <link href="./dist/css/demo.min.css?1692870487" rel="stylesheet"/>
+    <link href="./dist/css/tabler.min.css?1692870487" rel="stylesheet" />
+    <link href="./dist/css/tabler-flags.min.css?1692870487" rel="stylesheet" />
+    <link href="./dist/css/tabler-payments.min.css?1692870487" rel="stylesheet" />
+    <link href="./dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet" />
+    <link href="./dist/css/demo.min.css?1692870487" rel="stylesheet" />
     <style>
-      @import url('https://rsms.me/inter/inter.css');
-      :root {
-      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
-      }
-      body {
-      	font-feature-settings: "cv03", "cv04", "cv11";
-      }
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
     </style>
-  </head>
-  <body  class=" d-flex flex-column">
+</head>
+
+<body class=" d-flex flex-column">
     <script src="./dist/js/demo-theme.min.js?1692870487"></script>
     <div class="page page-center">
-      <div class="container container-tight py-4">
-        <form class="card card-md" action="{{ route('register') }}" method="post" x-data="formSubmit" @submit.prevent="submit" autocomplete="off">
-        @csrf
-          <div class="card-body">
-            <h2 class="h2 text-center mb-4">Create New Account</h2>
+        <div class="container container-tight py-4">
+            <form class="card card-md" action="{{ route('register') }}" method="post" x-data="formSubmit"
+                @submit.prevent="submit" autocomplete="off">
+                @csrf
+                <div class="card-body">
+                    <h2 class="h2 text-center mb-4">Create New Account</h2>
 
-            {{-- name --}}
-            <div class="mb-3">
-              <label class="form-label" for="name">Name</label>
-              <input type="text" name="name" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror">
-              @error('name')
-                <div class="invalid-feedback">{{ $message }}</div>
-             @enderror
+                    {{-- name --}}
+                    <div class="mb-3">
+                        <label class="form-label" for="name">Name</label>
+                        <input type="text" name="name" value="{{ old('name') }}"
+                            class="form-control @error('name') is-invalid @enderror">
+                        @error('name')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
 
+                    </div>
+
+                    {{-- Email --}}
+                    <div class="mb-3">
+                        <label class="form-label" for="email">Email</label>
+                        <input type="text" name="email" value="{{ old('email') }}"
+                            class="form-control @error('email') is-invalid @enderror">
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+
+                    </div>
+
+                    {{-- password --}}
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <div class="input-group input-group-flat">
+                            <input type="password" name="password"
+                                class="form-control @error('password') is-invalid @enderror">
+                            @error('password')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    {{-- confirm password --}}
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Confirm Password</label>
+                        <div class="input-group input-group-flat">
+                            <input type="password" name="password_confirmation"
+                                class="form-control @error('password_confirmation') is-invalid @enderror">
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-check">
+                            <input type="checkbox" class="form-check-input" />
+                            <span class="form-check-label">Agree the <a href="./terms-of-service.html"
+                                    tabindex="-1">terms and policy</a>.</span>
+                        </label>
+                    </div>
+                    <div class="form-footer">
+                        <button type="submit" class="btn btn-primary w-100">Create new account</button>
+                    </div>
+                </div>
+            </form>
+            <div class="text-center text-secondary mt-3">
+                Already have account? <a href="{{ route('login') }}" tabindex="-1">Sign in</a>
             </div>
-
-            {{-- Email --}}
-            <div class="mb-3">
-                <label class="form-label" for="email">Email</label>
-              <input type="text" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror">
-              @error('email')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-
-            </div>
-
-            {{-- password --}}
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-              <div class="input-group input-group-flat">
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
-                @error('password')
-                <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-              </div>
-            </div>
-
-            {{-- confirm password --}}
-            <div class="mb-3">
-                <label for="password_confirmation" class="form-label">Confirm Password</label>
-              <div class="input-group input-group-flat">
-                <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror">
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label class="form-check">
-                <input type="checkbox" class="form-check-input"/>
-                <span class="form-check-label">Agree the <a href="./terms-of-service.html" tabindex="-1">terms and policy</a>.</span>
-              </label>
-            </div>
-            <div class="form-footer">
-              <button type="submit" class="btn btn-primary w-100">Create new account</button>
-            </div>
-          </div>
-        </form>
-        <div class="text-center text-secondary mt-3">
-          Already have account? <a href="{{ route('login') }}" tabindex="-1">Sign in</a>
         </div>
-      </div>
     </div>
     <!-- Libs JS -->
     <!-- Tabler Core -->
     <script src="./dist/js/tabler.min.js?1692870487" defer></script>
     <script src="./dist/js/demo.min.js?1692870487" defer></script>
-  </body>
+</body>
+
 </html>
