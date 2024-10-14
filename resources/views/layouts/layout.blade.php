f1c3b0350dca1c41cd5e1ba39cfa75aa6d4ab7f9
davinanurul <davinanurul06@gmail.com>
2024-10-14 10:52:32 +0700

    memperbaiki layout form add data perusahaan

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -329,6 +329,16 @@
                         </a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" href="{{ route('company.index')}}">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-building-skyscraper"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l18 0" /><path d="M5 21v-14l8 -4v18" /><path d="M19 21v-10l-6 -4" /><path d="M9 9l0 .01" /><path d="M9 12l0 .01" /><path d="M9 15l0 .01" /><path d="M9 18l0 .01" /></svg>
+                            </span>
+                            <span class="nav-link-title">
+                                Data Perusahaan
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="{{ route('branches.index') }}">
                             <span class="nav-link-icon d-md-none d-lg-inline-block">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-building-community">
@@ -459,8 +469,8 @@
                                     <a href="{{ route('profile.show') }}" class="dropdown-item">
                                         <i class="fas fa-user me-2"></i> Profil
                                     </a>
-                                    <a href="{{ route('company.index')}}" class="dropdown-item">
-                                        <i class="fas fa-cog me-2"></i> Profil Perusahaan
+                                    <a href="#" class="dropdown-item">
+                                        <i class="fas fa-cog me-2"></i> Pengaturan
                                     </a>
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
