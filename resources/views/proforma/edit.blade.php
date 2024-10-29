357fa36b27e9c3936761117536a19da13c5ed4c3
Dede Nugroho <dedenugrohomm2@gmail.com>
2024-10-29 11:43:36 +0700

    mencegah user mengganti product pada edit proforma invoice

--- resources/views/proforma/edit.blade.php
+++ resources/views/proforma/edit.blade.php
@@ -253,18 +253,25 @@
                                                                 <span>:</span>
                                                             </div>
                                                             <div class="col-5">
-                                                                <select class="form-control product" id="product"
-                                                                    name="id_product" required>
-                                                                    <option value="">Pilih Product</option>
-                                                                    @foreach ($products as $product)
-                                                                        <option value="{{ $product->id }}"
-                                                                            data-code="{{ $product->code }}"
-                                                                            data-abbreviation="{{ $product->abbreviation }}"
-                                                                            {{ $product->id == $productSelectedID ? 'selected' : '' }}>
-                                                                            {{ $product->name }}
-                                                                        </option>
-                                                                    @endforeach
-                                                                </select>
+                                                                <div class="d-flex align-items-center">
+                                                                    <select class="form-control product" id="product"
+                                                                        name="id_product" required>
+                                                                        <option value="">Pilih Product</option>
+                                                                        @foreach ($products as $product)
+                                                                            <option value="{{ $product->id }}"
+                                                                                data-code="{{ $product->code }}"
+                                                                                data-abbreviation="{{ $product->abbreviation }}"
+                                                                                {{ $product->id == $productSelectedID ? 'selected' : '' }}>
+                                                                                {{ $product->name }}
+                                                                            </option>
+                                                                        @endforeach
+                                                                    </select>
+                                                                    <button id="infoButton" class="btn btn-warning btn-sm ms-2"
+                                                                        title="Informasi">
+                                                                        <i class="fas fa-info-circle"></i>
+                                                                        <!-- Using Font Awesome icon -->
+                                                                    </button>
+                                                                </div>
                                                                 <span class="error-message" id="product_error"
                                                                     style="color: red; display: none;"></span>
                                                             </div>
@@ -989,6 +996,18 @@
                     }
                 });
             }
+
+            // Optional: Handle click on info button to show the alert
+            $('#infoButton').on('click', function(event) {
+                event.preventDefault(); // Prevent default action (just in case)
+                Swal.fire({
+                    title: 'Informasi!',
+                    text: 'Kosongkan detail transaksi sebelumnya untuk mengganti produk.',
+                    icon: 'info',
+                    showCloseButton: true,
+                    confirmButtonText: 'Tutup',
+                });
+            });
 
             function addDynamicEventListeners() {
                 // Event listener for qty and carton input changes in #loadedData (related to formDetailTransaction)
