3b8eec8262350cef7102b78271c2d10dc29d10c4
Dede Nugroho <dedenugrohomm2@gmail.com>
2024-11-11 20:04:10 +0700

    membuat input buyer name

--- app/Http/Controllers/BillOfPaymentController.php
+++ app/Http/Controllers/BillOfPaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BillOfPayment;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,24 @@ class BillOfPaymentController extends Controller
 
     public function create()
     {
-        return view('bill-of-payments.create');
+        // Mengambil number terakhir dari tabel transaction
+        $lastBillOfDateNo = BillOfPayment::orderBy('no_inv', 'desc')->first();
+
+        // Jika belum ada data di kolom number, mulai dari 0001
+        if ($lastBillOfDateNo === null || empty($lastBillOfDateNo->no_inv)) {
+            $newNumber = '0001';
+        } else {
+            // Mengambil number terakhir dan menambah 1, pastikan tetap 4 digit
+            $lastNumber = intval($lastBillOfDateNo->no_inv);
+            $newNumber = str_pad($lastNumber + 1, 4, '0', STR_PAD_LEFT);
+        }
+
+        // Mengambil dua digit tanggal saat ini
+        $twoDigitMonth = date('m');
+
+        // Menggabungkan $newNumber dengan dua digit tanggal
+        $formattedNumber = $newNumber . '/' . $twoDigitMonth;
+
+        return view('bill-of-payments.create', compact('formattedNumber'));
     }
 }
--- resources/views/bill-of-payments/create.blade.php
+++ resources/views/bill-of-payments/create.blade.php
@@ -36,47 +36,65 @@
                                         <div class="row">
                                             <div class="col-6">
                                                 <div class="row">
-                                                    <div class="col-6">
+                                                    <div class="col-4">
                                                         <p>Month</p>
                                                     </div>
                                                     <div class="col-1 text-center">
                                                         <span>:</span>
                                                     </div>
-                                                    <div class="col-5">
+                                                    <div class="col-7">
                                                         <p>{{ date('F Y') }}</p>
                                                     </div>
                                                 </div>
                                                 <div class="row">
-                                                    <div class="col-6">
+                                                    <div class="col-4">
                                                         <p>No. Inv</p>
                                                     </div>
                                                     <div class="col-1 text-center">
                                                         <span>:</span>
                                                     </div>
-                                                    <div class="col-5">
+                                                    <div class="col-7">
                                                         <p id="no-inv-display">-</p>
                                                     </div>
                                                 </div>
                                                 <div class="row">
-                                                    <div class="col-6">
+                                                    <div class="col-4">
                                                         <p>Buyer Name</p>
                                                     </div>
                                                     <div class="col-1 text-center">
                                                         <span>:</span>
                                                     </div>
-                                                    <div class="col-5">
-                                                        <p id="numberDisplay">-</p>
+                                                    <div class="col-7">
+                                                        <div class="form-group">
+                                                            <div class="input-group">
+                                                                <input type="text" class="form-control"
+                                                                    id="selectedClientName" placeholder="Pilih Client"
+                                                                    readonly>
+                                                                <input type="hidden" id="selectedClientId"
+                                                                    name="id_client">
+                                                                <div class="btn-group">
+                                                                    <button type="button" class="btn btn-primary btn-md"
+                                                                        data-bs-toggle="modal"
+                                                                        data-bs-target="#clientsModal">
+                                                                        <i data-feather="search"></i> Cari
+                                                                    </button>
+                                                                </div>
+                                                            </div>
+                                                            <span class="error-message" id="selectedClientId_error"
+                                                                style="color: red; display: none;"></span>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="row">
-                                                    <div class="col-6">
+                                                <div class="row mt-2">
+                                                    <div class="col-4">
                                                         <p>Company Name</p>
                                                     </div>
                                                     <div class="col-1 text-center">
                                                         <span>:</span>
                                                     </div>
-                                                    <div class="col-5">
-                                                        <p id="numberDisplay">-</p>
+                                                    <div class="col-7">
+                                                        <input type="text" class="form-control" id="selectedClientCompanyName"
+                                                            placeholder="Nama Perusahaan" readonly>
                                                     </div>
                                                 </div>
                                             </div>
@@ -86,7 +104,7 @@
 
                                 <div class="row mt-4">
                                     <div class="col-md-12">
-                                        <table class="table card-table table-vcenter text-nowrap" id="clientsModalTable">
+                                        <table class="table card-table table-vcenter text-nowrap">
                                             <thead>
                                                 <tr>
                                                     <th class="text-center">No</th>
@@ -101,7 +119,6 @@
                                             </thead>
                                             <tbody>
                                             </tbody>
-                                        </table>
                                         </table>
                                     </div>
                                 </div>
@@ -185,4 +202,137 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade text-left" id="clientsModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="clientModalLabel">Pilih Client</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table card-table table-vcenter text-nowrap" id="clientsModalTable">
+                        <thead>
+                            <tr>
+                                <th class="text-center">No</th>
+                                <th class="text-center">Nama</th>
+                                <th class="text-center">Nama Perusahaan</th>
+                                <th class="text-center">Alamat</th>
+                                <th class="text-center">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- server side data --}}
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-info" data-bs-dismiss="modal"
+                        aria-label="Close">Tutup</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('#clientsModalTable').DataTable({
+                autoWidth: false,
+                processing: false,
+                serverSide: true,
+                ajax: "{{ route('clients.index') }}",
+                columns: [{
+                        data: null,
+                        class: 'text-center',
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        },
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'name',
+                        name: 'name',
+                        class: 'text-center'
+                    },
+                    {
+                        data: 'company_name',
+                        name: 'company_name',
+                        class: 'text-center'
+                    },
+                    {
+                        data: 'address',
+                        name: 'address',
+                        class: 'text-center'
+                    },
+                    {
+                        data: null,
+                        render: function(data, type, row) {
+                            return `<button class="btn btn-primary select-client" data-id="${row.id}" data-name="${row.name}" data-company="${row.company_name}">Pilih</button>`;
+                        },
+                        class: 'text-center',
+                        orderable: false,
+                        searchable: false
+                    }
+                ],
+                language: {
+                    lengthMenu: "Tampilkan _MENU_ entri",
+                    info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
+                    paginate: {
+                        first: "Pertama",
+                        last: "Terakhir",
+                        next: "Selanjutnya",
+                        previous: "Sebelumnya"
+                    },
+                    search: "Cari :",
+                    infoFiltered: "(disaring dari total _MAX_ entri)"
+                },
+                lengthMenu: [5, 10, 25, 50],
+                pageLength: 10,
+                drawCallback: function() {
+                    $('#clientsModalTable td:nth-child(2), #clientsModalTable th:nth-child(2)').css({
+                        'max-width': '200px',
+                        'white-space': 'normal',
+                        'word-wrap': 'break-word'
+                    });
+                    $('#clientsModalTable td:nth-child(3), #clientsModalTable th:nth-child(3)').css({
+                        'max-width': '280px',
+                        'overflow': 'hidden',
+                        'text-overflow': 'ellipsis'
+                    });
+                }
+            });
+
+            // Event listener untuk tombol "Pilih" di tabel client
+            $('#clientsModalTable tbody').on('click', '.select-client', function() {
+                var clientId = $(this).data('id');
+                var clientName = $(this).data('name');
+                var companyName = $(this).data('company');
+
+                $('#selectedClientId').val(clientId);
+                $('#selectedClientName').val(clientName);
+                $('#selectedClientCompanyName').val(companyName);
+                $('#clientsModal').modal('hide');
+            });
+
+            function updateNumber() {
+                const currentDate = new Date();
+
+                // Dapatkan bulan dalam format angka Romawi
+                const romanMonths = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'];
+                const romanMonth = romanMonths[currentDate.getMonth()];
+
+                // Dapatkan tahun dalam format dua digit
+                const twoDigitYear = currentDate.getFullYear().toString().slice(-2);
+
+                const formattedNumber = '/INV/' + romanMonth + '/' +
+                    twoDigitYear;
+                const finalNumber = '{{ $formattedNumber }}' + formattedNumber;
+                $('#no-inv-display').text(finalNumber);
+            }
+
+            updateNumber();
+        });
+    </script>
 @endsection
