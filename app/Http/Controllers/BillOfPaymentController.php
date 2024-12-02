fba04377f0c16284b078629c1ebda83e44eab854
Dede Nugroho <dedenugrohomm2@gmail.com>
2024-12-02 09:57:14 +0700

    menambahkan 'id_client_company' => 'required|exists:client_company,id' pada fungsi create BillOfPaymentController.php

--- app/Http/Controllers/BillOfPaymentController.php
+++ app/Http/Controllers/BillOfPaymentController.php
@@ -130,6 +130,7 @@ class BillOfPaymentController extends Controller
             'month' => 'required',
             'no_inv' => 'required',
             'id_client' => 'required',
+            'id_client_company' => 'required|exists:client_company,id',
             'total' => 'required|numeric|gte:0',
         ], [
             'total.gte' => 'Nilai paid tidak boleh melebihi nilai bill.',
