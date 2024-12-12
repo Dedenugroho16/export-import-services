f78e5269b8e28eafc1a36b609c551eacc0c8a5a1
davinanurul <davinanurul06@gmail.com>
2024-12-12 22:08:07 +0700

    menambahkan button kembali di halaman impoer excel

--- resources/views/countries/import.blade.php
+++ resources/views/countries/import.blade.php
@@ -9,6 +9,9 @@
                     <div class="col-md-6">
                         <div class="card shadow-sm">
                             <div class="card-header bg-primary text-white text-center">
+                                <a href="{{ route('countries.index') }}" class="text-secondary" title="Kembali">
+                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left me-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
+                                </a> 
                                 <h4 class="mb-0">Import Data Excel</h4>
                             </div>
                             <div class="card-body">
