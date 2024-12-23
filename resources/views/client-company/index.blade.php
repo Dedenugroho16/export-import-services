7211ec284a8a07088bca97b4605d83b4c1e0d94c
Dede Nugroho <dedenugrohomm2@gmail.com>
2024-12-24 00:12:24 +0700

    menambahkan sweetalert succes pada import client company, dan client

--- resources/views/client-company/index.blade.php
+++ resources/views/client-company/index.blade.php
@@ -1,158 +1,186 @@
 @extends('layouts.layout')
 @section('title', 'Perusahaan Client')
 @section('content')
-<div class="page-body">
-    <div class="container-xl">
-        <!-- Header dan Tombol Tambah Komoditas -->
-        @if(in_array(auth()->user()->role, ['admin', 'operator']))
-            <div class="mb-4 align-items-center">
-                <a href="{{ route('client-companies.create') }}" class="btn btn-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-plus">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M12 5v14" />
-                        <path d="M5 12h14" />
-                    </svg>
-                    Tambah
-                </a>
-                <a href="{{ route('client-companies.import') }}" class="btn btn-warning ms-3">
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-                    Import Excel
-                </a>
-            </div>
-        @endif
+    <div class="page-body">
+        <div class="container-xl">
+            <!-- Header dan Tombol Tambah Komoditas -->
+            @if (in_array(auth()->user()->role, ['admin', 'operator']))
+                <div class="mb-4 align-items-center">
+                    <a href="{{ route('client-companies.create') }}" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="icon icon-tabler icon-tabler-plus">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 5v14" />
+                            <path d="M5 12h14" />
+                        </svg>
+                        Tambah
+                    </a>
+                    <a href="{{ route('client-companies.import') }}" class="btn btn-warning ms-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 5l0 14" />
+                            <path d="M5 12l14 0" />
+                        </svg>
+                        Import Excel
+                    </a>
+                </div>
+            @endif
 
-        <!-- Section Tabel Komoditas -->
-        <div class="row row-deck row-cards">
-            <div class="col-12">
-                <div class="card mb-5">
-                    <div class="card-body">
-                        <!-- Success Message for Deleting, Editing, or Adding Data -->
-                        @if (session('success'))
-                        <div class="alert alert-important alert-success alert-dismissible" role="alert">
-                            <div class="d-flex">
-                                <div>
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/check -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>
-                                </div>
-                                <div>
-                                    {{ session('success') }}
-                                </div>
+            <!-- Section Tabel Komoditas -->
+            <div class="row row-deck row-cards">
+                <div class="col-12">
+                    <div class="card mb-5">
+                        <div class="card-body">
+                            <!-- Success Message for Deleting, Editing, or Adding Data -->
+                            @if (session('success'))
+                                <script>
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Berhasil',
+                                        text: '{{ session('success') }}',
+                                        confirmButtonText: 'OK'
+                                    });
+                                </script>
+                            @endif
+                            <!-- Tabel Komoditas -->
+                            <div class="table-responsive">
+                                <table class="table card-table table-vcenter text-nowrap" id="clientCompanyTable">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">No</th>
+                                            <th class="text-center">Nama Perusahaan</th>
+                                            <th class="text-center">Alamat</th>
+                                            <th class="text-center">PO BOX</th>
+                                            <th class="text-center">Telepon</th>
+                                            <th class="text-center">Fax</th>
+                                            <th class="text-center">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
                             </div>
-                            <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                            <!-- Tabel Berakhir -->
                         </div>
-                        @endif
-                        <!-- Tabel Komoditas -->
-                        <div class="table-responsive">
-                            <table class="table card-table table-vcenter text-nowrap" id="clientCompanyTable">
-                                <thead>
-                                    <tr>
-                                        <th class="text-center">No</th>
-                                        <th class="text-center">Nama Perusahaan</th>
-                                        <th class="text-center">Alamat</th>
-                                        <th class="text-center">PO BOX</th>
-                                        <th class="text-center">Telepon</th>
-                                        <th class="text-center">Fax</th>
-                                        <th class="text-center">Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- Tabel Berakhir -->
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<!-- Script DataTables -->
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('#clientCompanyTable').DataTable({
-            processing: false,
-            serverSide: true,
-            ajax: "{{ route('client-companies.index') }}",
-            autoWidth: false,
-            columnDefs: [
-                { width: '200px', targets: 1 }
-            ],
-            columns: [
-                { 
-                    data: null, 
-                    class: 'text-center',
-                    render: function(data, type, row, meta) {
-                        return meta.row + meta.settings._iDisplayStart + 1;
+    <!-- Script DataTables -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#clientCompanyTable').DataTable({
+                processing: false,
+                serverSide: true,
+                ajax: "{{ route('client-companies.index') }}",
+                autoWidth: false,
+                columnDefs: [{
+                    width: '200px',
+                    targets: 1
+                }],
+                columns: [{
+                        data: null,
+                        class: 'text-center',
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        },
+                        orderable: false,
+                        searchable: false
                     },
-                    orderable: false,
-                    searchable: false
+                    {
+                        data: 'company_name',
+                        name: 'company_name'
+                    },
+                    {
+                        data: 'address',
+                        name: 'address'
+                    },
+                    {
+                        data: 'PO_BOX',
+                        name: 'PO_BOX',
+                        class: 'text-center'
+                    },
+                    {
+                        data: 'tel',
+                        name: 'tel',
+                        class: 'text-center'
+                    },
+                    {
+                        data: 'fax',
+                        name: 'fax',
+                        class: 'text-center'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false,
+                        class: 'text-center'
+                    }
+                ],
+                language: {
+                    lengthMenu: "Tampilkan _MENU_ Data",
+                    info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
+                    paginate: {
+                        first: "Pertama",
+                        last: "Terakhir",
+                        next: "Selanjutnya",
+                        previous: "Sebelumnya"
+                    },
+                    search: "Cari :",
+                    infoFiltered: "(disaring dari total _MAX_ entri)"
                 },
-                { data: 'company_name', name: 'company_name'},
-                { data: 'address', name: 'address' },
-                { data: 'PO_BOX', name: 'PO_BOX', class: 'text-center' },
-                { data: 'tel', name: 'tel', class: 'text-center' },
-                { data: 'fax', name: 'fax', class: 'text-center' },
-                { data: 'action', name: 'action', orderable: false, searchable: false, class: 'text-center' }
-            ],
-            language: {
-                lengthMenu: "Tampilkan _MENU_ Data",
-                info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
-                paginate: {
-                    first: "Pertama",
-                    last: "Terakhir",
-                    next: "Selanjutnya",
-                    previous: "Sebelumnya"
-                },
-                search: "Cari :",
-                infoFiltered: "(disaring dari total _MAX_ entri)"
-            },
-            lengthMenu: [5, 10, 25, 50], // Tentukan jumlah data yang ditampilkan per halaman
-            pageLength: 10,
-            drawCallback: function() {
-                $('#clientCompanyTable td:nth-child(2), #clientCompanyTable td:nth-child(3)').css({
-                    'max-width': '250px',
-                    'white-space': 'normal',
-                    'word-wrap': 'break-word'
-                });
-            }
-        });
-    });
-</script>
-
-<!-- Script Validasi Form -->
-<script>
-document.addEventListener('DOMContentLoaded', function () {
-    const forms = document.querySelectorAll('form');
-    forms.forEach(form => {
-        form.addEventListener('submit', function (e) {
-            const inputs = form.querySelectorAll('input, select, textarea');
-            let isValid = true;
-
-            inputs.forEach(input => {
-                if (input.required && !input.value.trim()) {
-                    isValid = false;
-                    input.classList.add('is-invalid');
-                } else {
-                    input.classList.remove('is-invalid');
+                lengthMenu: [5, 10, 25, 50], // Tentukan jumlah data yang ditampilkan per halaman
+                pageLength: 10,
+                drawCallback: function() {
+                    $('#clientCompanyTable td:nth-child(2), #clientCompanyTable td:nth-child(3)').css({
+                        'max-width': '250px',
+                        'white-space': 'normal',
+                        'word-wrap': 'break-word'
+                    });
                 }
             });
-
-            if (!isValid) {
-                e.preventDefault(); // Stop form from submitting
-                alert('Harap isi semua field yang dibutuhkan.');
-            }
         });
-    });
-});
+    </script>
 
-$(document).ready(function() {
-        setTimeout(function() {
-            $('.alert-dismissible').fadeOut();
-        }, 3000);
-    });
-</script>
+    <!-- Script Validasi Form -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const forms = document.querySelectorAll('form');
+            forms.forEach(form => {
+                form.addEventListener('submit', function(e) {
+                    const inputs = form.querySelectorAll('input, select, textarea');
+                    let isValid = true;
+
+                    inputs.forEach(input => {
+                        if (input.required && !input.value.trim()) {
+                            isValid = false;
+                            input.classList.add('is-invalid');
+                        } else {
+                            input.classList.remove('is-invalid');
+                        }
+                    });
+
+                    if (!isValid) {
+                        e.preventDefault(); // Stop form from submitting
+                        alert('Harap isi semua field yang dibutuhkan.');
+                    }
+                });
+            });
+        });
+
+        $(document).ready(function() {
+            setTimeout(function() {
+                $('.alert-dismissible').fadeOut();
+            }, 3000);
+        });
+    </script>
 
 @endsection
